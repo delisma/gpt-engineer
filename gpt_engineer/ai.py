cb1ee30c30da2b5ad1e490febe9ca5391cdca9ae
delisma <david.elisma@tbs-sct.gc.ca>
2023-06-27 17:03:53 -0400

    Update ai.py

--- gpt_engineer/ai.py
+++ gpt_engineer/ai.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 class AI:
-    def __init__(self, model="gpt-4", temperature=0.1):
+    def __init__(self, model="gpt-3.5-turbo-16k", temperature=0.1):
         self.temperature = temperature
         self.model = model
 
